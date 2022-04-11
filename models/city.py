00e532f8e697d3cea7e2a7726601fc121828220e
Mateo Victorica <3951@holbertonschool.com>
2022-04-11 19:06:24 -0300

    ex city and state

--- models/city.py
+++ models/city.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from tokenize import String
+from sqlalchemy import Column, ForeignKey
+from models.base_model import Base, BaseModel
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(ForeignKey('states.id'), String(60), nullable=False)
+    state = relationship("State")
--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from os import stat_result
+from tokenize import String
+
+from sqlalchemy import Column
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
     name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan")
+    
