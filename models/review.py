0ebd8bf87e80d7dfb5f4a32b43909c9cc173e41f
Mateo Victorica <3951@holbertonschool.com>
2022-04-12 20:21:52 -0300

    ex9

--- models/review.py
+++ models/review.py
@@ -9,4 +9,4 @@ class Review(BaseModel):
     __tablename__ = "reviews"
     place_id = Column(String(60), ForeignKey('places.id', nullable=False))
     user_id = Column(String(60), ForeignKey('users.id', nullable=False))
-    text = Column(String(1024), nuallable=False)
+    text = Column(String(1024), nullable=False)
