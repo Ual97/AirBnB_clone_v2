59901ec9e057c0ae2c722cd0bbb5d3cd8caced36
Mateo Victorica <3951@holbertonschool.com>
2022-04-12 14:20:30 -0300

    changed db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -60,7 +60,7 @@ class DBStorage:
         from models.user import User
         from models.review import Review
         from models.base_model import BaseModel
-        self.__session = Base.metadata.create_all(self.__engine)
+        Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
