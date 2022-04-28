6bd2a96bbbc860a648b6845db11034174cfe1ecf
Ual97 <3951@holbertonschool.com>
2022-04-28 12:27:59 -0700

    ex 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#set up servers for deplyoment of web_static
+
+ADD_WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "35i $ADD_WEBSTATIC" /etc/nginx/sites-available/default
+sudo service nginx start
