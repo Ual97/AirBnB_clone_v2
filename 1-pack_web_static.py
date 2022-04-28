8a6da879b83cf43f31f74337a54cd15066b00634
Ual97 <3951@holbertonschool.com>
2022-04-28 12:48:02 -0700

    ex 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fabric script that generates a.tgz archive with contents
+ from webstatic folder"""
+
+from fabric.api import local
+from time import strftime
+
+def do_pack():
+    """generates .tgz file"""
+    time = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        filename = "versions/web_static_{}.tgz".format(time)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except:
+        return None
