ada1d150e20ce15a567874299ddf54046c019c6b
Ual97 <3951@holbertonschool.com>
2022-05-15 14:25:05 -0700

    ex 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app with more custom routes for c and python and int
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display text"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """display custom text given"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def text_if_int(n):
+    """display text if given int"""
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_if_int(n):
+    """display html page if given int"""
+    return render_template('5-number.html', n=n)a
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def html_odd_or_even(n):
+    """display html page only if int given, depends if int is odd or even"""
+    odd_or_even = "even" if (n % 2 == 0) else "odd"
+    return render_template('6-number_odd_or_even.html',
+                           n=n, odd_or_even=odd_or_even)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
